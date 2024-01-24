00b31e1a5c2900c687d321a7d0c6693176fbfc1f
shibotgit <helloakaa1@gmail.com>
2024-01-24 11:32:37 +0000

    added caddy file

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ console.info("Authenticating...");
 
 CrispClient.authenticateTier("plugin", TOKEN.identifier, TOKEN.key);
 CrispClient.setRtmMode(Crisp.RTM_MODES.WebSockets);
-const socket = io("http://127.0.0.1:5000");
+const socket = io("http://159.223.195.100");
 
 console.info("Listening for events...");
 
